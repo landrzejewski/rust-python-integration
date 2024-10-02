75419dacc9cfd6e58cf9d7e56829e1cadd47e011
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2024-10-02 05:49:13 +0200

    fix

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 // python3 -m maturin init
-// python3 -m maturin devlop
+// python3 -m maturin build
 // pip3 install localization-0.1.0-cp39-cp39-macosx_11_0_arm64.whl
 
 use pyo3::prelude::*;
@@ -13,12 +13,4 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
 #[pyfunction]
 fn sum(a: i32, b: i32) -> i32 {
     a + b
-}
-
-/// A Python module implemented in Rust.
-#[pymodule]
-fn localization(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
-    m.add_function(wrap_pyfunction!(sum, m)?)?;
-    Ok(())
 }
